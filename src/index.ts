e77677e08bef7af430d527c4f4f8e38d4157fd78
UnicornDad8 <unicorndad8@gmail.com>
2024-05-16 13:26:00 -0400

    Working with objects, retire function

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,15 @@
 // object
-let employee: { readonly id: number; name: string } = { id: 1, name: "Ceci" };
+let employee: {
+  readonly id: number;
+  name: string;
+  retire: (date: Date) => void;
+} = {
+  id: 1,
+  name: "Ceci",
+  retire: (date: Date) => {
+    console.log(date);
+  },
+};
 
 // typescript compiler prevent us from accesing the id property
 // employee.id = 0;
