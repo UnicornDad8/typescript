21589baef4d1354ac2e71277192c4e4adbce8084
UnicornDad8 <unicorndad8@gmail.com>
2024-05-16 13:14:54 -0400

    Making object property readonly

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
 // object
-let employee: { id: number; name: string } = { id: 1, name: "Ceci" };
+let employee: { readonly id: number; name: string } = { id: 1, name: "Ceci" };
+
+// typescript compiler prevent us from accesing the id property
+// employee.id = 0;
