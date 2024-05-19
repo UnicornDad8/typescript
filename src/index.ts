6d307907ae5e38dfa8b1e18d1dbe03b9a107e260
UnicornDad8 <unicorndad8@gmail.com>
2024-05-19 13:22:31 -0400

    Optional chaining

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,21 @@
-// Nullable types
-function greet(name: string | null) {
-  if (name) console.log(name.toUpperCase());
-  else console.log("Hola");
+// Optional chaining
+type Customer = {
+  birthday?: Date;
+};
+
+function getCustomer(id: number): Customer | null | undefined {
+  return id === 0 ? null : { birthday: new Date() };
 }
 
-greet(null);
+let customer = getCustomer(1);
+
+// Optional property access operator
+console.log(customer?.birthday?.getFullYear());
+
+// Optional element access operator
+// if(customers !== null && customers !== undefined)
+// customers?.[0]
+
+// Optional call
+let log: any = null;
+log?.("a");
