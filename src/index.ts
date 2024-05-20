2d51577746239f6208b7e4675370bc37d18d27c1
UnicornDad8 <unicorndad8@gmail.com>
2024-05-20 11:48:06 -0400

    Learning about the never type

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,17 @@
-// The unknown type
-function render(document: unknown) {
-  if (typeof document === "string") {
-    document.toUpperCase();
+function reject(message: string): never {
+  throw new Error(message);
+}
+
+// The never type
+function processEvents(): never {
+  while (true) {
+    // Read message from a queue
   }
 }
+
+// processEvents();
+reject("...");
+
+// this line is unreachable because the process never return
+// because there is a while infinite loop
+console.log("hello world");
