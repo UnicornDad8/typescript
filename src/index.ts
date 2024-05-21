cf2a0a14465cdc7a1465d836d3ba9d79f88ca4f5
UnicornDad8 <unicorndad8@gmail.com>
2024-05-21 13:28:45 -0400

    Creating a bank account class in typescript, with id, owner and balance properties

--- src/index.ts
+++ src/index.ts
@@ -1,65 +1,18 @@
-// Exercises of this section
+// classes are blueprints of objects
+class Account {
+  id: number;
+  owner: string;
+  balance: number;
 
-type User = {
-  name: string;
-  age: number;
-  occupation?: string;
-};
+  constructor(id: number, owner: string, balance: number) {
+    this.id = id;
+    this.owner = owner;
+    this.balance = balance;
+  }
 
-// make an alias for this type
-let users = [
-  { name: "John Smith", age: 30, occupation: "Software engineer" },
-  { name: "Kate Müller", age: 28 },
-];
+  deposit(amount: number): void {
+    if (amount <= 0) throw new Error("Invalid amount");
 
-// Birds fly. Fish swim.
-// A Pet can be a Bird or Fish.
-// Use type aliases to represent these
-type Bird = {
-  fly: () => void;
-};
-
-type Fish = {
-  swim: () => void;
-};
-
-type Pet = Bird | Fish;
-
-// Define a type for representing the days of week.
-// Valid values are “Monday”, “Tuesday”, etc.
-
-type DayOfTheWeek =
-  | "Monday"
-  | "Tuesday"
-  | "Wednesday"
-  | "Thursday"
-  | "Friday"
-  | "Saturday"
-  | "Sunday";
-
-let day: DayOfTheWeek = "Monday";
-
-// Simplify the following code snippets
-let user = getUser();
-
-console.log(user && user.address ? user.address.street : undefined);
-
-// ---------------------------------------------------------------
-// Answer:
-console.log(user?.address?.street);
-
-let x = foo !== null && foo !== undefined ? foo : bar();
-
-// ---------------------------------------------------------------
-// Answer:
-let x = foo ?? bar();
-
-// What is the problem in this piece of code?
-let value: unknown = "a";
-console.log(value.toUpperCase());
-
-// value is declared as an unknown type.
-// In order to call methods on an unknown object,
-// we have to use type narrowing first.
-let value: unknown = "a";
-if (typeof value === "string") console.log(value.toUpperCase());
+    this.balance += amount;
+  }
+}
