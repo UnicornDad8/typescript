5b52d98b1388fcf58ee0f6ce47f61b69e5ceb421
UnicornDad8 <unicorndad8@gmail.com>
2024-05-22 13:10:40 -0400

    Understanding static properties and methods on typescript

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,28 @@
-class SeatAssignment {
-  // A1, A2, ...
-  // Ceci, Peke, ...
-  // Index signature property
-  [seatNumber: string]: string;
+// imagine we are building the class for an Uber ride
+class Ride {
+  // passenger
+  // pickupLocation
+  // dropOffLocation
+  private static _activeRides: number = 0;
+
+  start() {
+    Ride._activeRides++;
+  }
+  stop() {
+    Ride._activeRides--;
+  }
+
+  // as a best practice getters for static properties
+  // are also declared as static methods
+  static get activeRides() {
+    return Ride._activeRides;
+  }
 }
 
-let seats = new SeatAssignment();
+let ride1 = new Ride();
+ride1.start();
 
-seats.A1 = "Ceci";
-seats.A2 = "Peke";
-seats.A3 = "Mosh";
+let ride2 = new Ride();
+ride2.start();
+
+console.log(Ride.activeRides);
