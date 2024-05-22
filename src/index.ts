a7b725983921eb84f49b99b40b92ba7b02a24fea
UnicornDad8 <unicorndad8@gmail.com>
2024-05-22 12:53:37 -0400

    Studying index signatures

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,12 @@
-// classes are blueprints of objects
-class Account {
-  // readonly id: number;
-  // owner: string;
-  // private _balance: number;
-  nickname?: string;
-
-  constructor(
-    public readonly id: number,
-    public owner: string,
-    private _balance: number
-  ) {
-    // this.id = id;
-    // this.owner = owner;
-    // this._balance = balance;
-  }
-
-  deposit(amount: number): void {
-    if (amount <= 0) throw new Error("Invalid amount");
-
-    this._balance += amount;
-  }
-
-  // getter
-  get balance(): number {
-    return this._balance;
-  }
-
-  // setter
-  /*
-  set balance(value: number) {
-    if (value < 0) throw new Error("Invalid value");
-
-    this._balance = value;
-  }
-  */
+class SeatAssignment {
+  // A1, A2, ...
+  // Ceci, Peke, ...
+  // Index signature property
+  [seatNumber: string]: string;
 }
 
-let account = new Account(1, "Ceci", 0);
+let seats = new SeatAssignment();
 
-account.deposit(100);
-console.log(account.balance);
+seats.A1 = "Ceci";
+seats.A2 = "Peke";
+seats.A3 = "Mosh";
