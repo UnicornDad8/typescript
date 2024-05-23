dbc1a61743230226935653d4041b566f3ba2fcd9
UnicornDad8 <unicorndad8@gmail.com>
2024-05-23 11:09:44 -0400

    Understanding the keyof operator

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,29 @@
-// Extending Generic classes
+// The "keyof" operator
 interface Product {
   name: string;
   price: number;
 }
 
+// if T is Product, keyof T is:
+// T => "name" | "price"
 class Store<T> {
   protected _objects: T[] = [];
 
   add(obj: T): void {
     this._objects.push(obj);
   }
-}
 
-// Pass on the generic type parameter
-class CompressibleStore<T> extends Store<T> {
-  compress() {}
-}
-
-// Restricting the generic type parameter
-class SearchableStore<T extends { name: string }> extends Store<T> {
-  find(name: string): T | undefined {
-    return this._objects.find((obj) => obj.name === name);
+  find(property: keyof T, value: unknown): T | undefined {
+    return this._objects.find((obj) => obj[property] === value);
   }
 }
 
-// Fix the generic type parameter
-class ProductStore extends Store<Product> {
-  filterByCategory(category: string): Product[] {
-    return [];
-  }
-}
+let store = new Store<Product>();
 
-let store = new CompressibleStore<Product>();
+store.add({ name: "apple", price: 1 });
 
-// passing, restricting or fixing the generic type
-// according to case scenario
+console.log(store.find("name", "apple"));
+console.log(store.find("price", 1));
+
+// uncomment below to see the typescript error
+// console.log(store.find("nonExistingProperty", 1));
