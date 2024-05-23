4c3cd707cb6a5bccee53c5dabb9a1a980741684b
UnicornDad8 <unicorndad8@gmail.com>
2024-05-23 05:13:38 -0400

    Studying polymorphic behavior on typescript

--- src/index.ts
+++ src/index.ts
@@ -27,5 +27,21 @@ class Teacher extends Person {
   }
 }
 
-let teacher = new Teacher("John", "Smith");
-console.log(teacher.fullName);
+class Principal extends Person {
+  override get fullName() {
+    return "Principal " + super.fullName;
+  }
+}
+
+printNames([
+  new Student(1, "John", "Smith"),
+  new Teacher("Mosh", "Hamedani"),
+  new Principal("Mary", "Smith"),
+]);
+
+// Polymorphic behavior
+function printNames(people: Person[]) {
+  for (let person of people) console.log(person.fullName);
+}
+
+// classes should be open to extension and closed for modification
