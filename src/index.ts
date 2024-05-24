b90603527df8ace8d86d2bcf36f040f6f99e1283
UnicornDad8 <unicorndad8@gmail.com>
2024-05-24 09:54:13 -0400

    Parametarized decorator

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,19 @@
-// class decorators
-function Component(constructor: Function) {
-  console.log("Component decorator called");
-  constructor.prototype.uniqueId = Date.now();
-  constructor.prototype.insertInDOM = () => {
-    console.log("Adding component to the DOM");
+// parametarized decorators
+type ComponentOptions = {
+  selector: string;
+};
+
+function Component(options: ComponentOptions) {
+  return (constructor: Function) => {
+    console.log("Component decorator called");
+    constructor.prototype.options = options;
+    constructor.prototype.uniqueId = Date.now();
+    constructor.prototype.insertInDOM = () => {
+      console.log("Adding component to the DOM");
+    };
   };
 }
 
 // this is similar behavior as extending from a based class
-@Component
+@Component({ selector: "#my-profile" }) // passing object as an argument
 class ProfileComponent {}
