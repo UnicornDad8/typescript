7e38f4504913715d3facc23e05fc732fd2ba8973
UnicornDad8 <unicorndad8@gmail.com>
2024-05-26 13:03:39 -0400

    method decorators, function expresion and PropertyDescriptor

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,19 @@
-// decorator composition
-type ComponentOptions = {
-  selector: string;
-};
-
-function Component(options: ComponentOptions) {
-  return (constructor: Function) => {
-    console.log("Component decorator called");
-    constructor.prototype.options = options;
-    constructor.prototype.uniqueId = Date.now();
-    constructor.prototype.insertInDOM = () => {
-      console.log("Adding component to the DOM");
-    };
+// method decorators
+function Log(target: any, methodName: string, descriptor: PropertyDescriptor) {
+  const original = descriptor.value as Function;
+  descriptor.value = function (...args: any) {
+    console.log("Before");
+    original.call(this, ...args);
+    console.log("After");
   };
 }
 
-function Pipe(constructor: Function) {
-  console.log("Pipe decorator called");
-  constructor.prototype.pipe = true;
+class Person {
+  @Log
+  say(message: string) {
+    console.log("Person says: " + message);
+  }
 }
 
-// this is similar behavior as extending from a based class
-@Component({ selector: "#my-profile" }) // passing object as an argument
-@Pipe
-class ProfileComponent {}
+let person = new Person();
+person.say("hello, blue sky");
